10c88252c8ae100e49175dc51b736b73deb8d84f
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-23 16:48:41 +0100

    Update Fig. 2.4 to SFS 2.4.0

--- 02_theory_of_sound_field_synthesis/fig2_04/fig2_04.m
+++ 02_theory_of_sound_field_synthesis/fig2_04/fig2_04.m
@@ -10,14 +10,12 @@ xs = [1 1 0];
 f = 800;
 
 %% ===== Toolbox settings ================================================
+conf = SFS_config;
 conf.resolution = 1000;
 conf.xref = [0 1 0];
-conf.plot.useplot = false;
-conf.c = 343;
-conf.phase = 0;
-conf.usenormalisation = true;
-conf.showprogress = true;
 
 %% ===== Main ============================================================
 [P,x,y,z] = sound_field_mono_plane_wave(X,Y,Z,xs,f,conf);
 gp_save_matrix('data/sound_field_plane_wave.dat',x,y,real(P));
+
+rmpath('../../matlab');
