4765d82f935c7ca983acc94f88dfea1867ba9bda
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-24 09:56:18 +0100

    Update Fig. 2.10 to SFS 2.4.0

--- 02_theory_of_sound_field_synthesis/fig2_10/fig2_10.m
+++ 02_theory_of_sound_field_synthesis/fig2_10/fig2_10.m
@@ -11,16 +11,9 @@ src = 'ps';
 f = 1000; % / Hz
 
 %% ===== Toolbox settings ================================================
+conf = SFS_config;
 conf.resolution = 1000; % / samples
-conf.xref = [0 0 0]; % / m
-conf.dimension = '2.5D';
-conf.c = 343; % / m/s
-conf.driving_functions = 'default';
-conf.phase = 0; % / rad
-conf.usenormalisation = true;
-conf.plot.useplot = false;
 conf.usetapwin = false;
-conf.tapwinlen = 0.3;
 conf.showprogress = true;
 
 %% ===== Secondary Sources ===============================================
@@ -28,16 +21,16 @@ conf.secondary_sources.size = 3; % / m
 conf.secondary_sources.center = [0 0 0]; % / m
 conf.secondary_sources.geometry = 'circle';
 conf.secondary_sources.number = 1000;
-conf.secondary_sources.x0 = [];
-% get secondary sources and store them, to do this calculation only once
-x0 = secondary_source_positions(conf);
-conf.secondary_sources.x0 = x0;
-% store complete array and repeat the first loudspeaker in order to draw a
+% Store complete array and repeat the first loudspeaker in order to draw a
 % complete circle
+x0 = secondary_source_positions(conf);
 gp_save_loudspeakers('data/array.txt',[x0; x0(1,:)]);
 
 %% ===== Wave Field Synthesis ============================================
 [P,x,y,~,x0] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
+P = normalization(P,'point');
 file = sprintf('data/sound_field_wfs_ps_f%iHz_nls%i.dat',f,conf.secondary_sources.number);
 gp_save_matrix(file,x,y,real(P));
 gp_save_loudspeakers('data/array_wfs_ps.txt',x0);
+
+rmpath('../../matlab');
