2f527b3d36849e71db17019586cb6449310e7b54
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-24 09:24:38 +0100

    Update Fig. 2.6 to SFS 2.4.0

--- 02_theory_of_sound_field_synthesis/fig2_06/fig2_06.m
+++ 02_theory_of_sound_field_synthesis/fig2_06/fig2_06.m
@@ -10,14 +10,14 @@ xs = [0 0 0];
 f = 800;
 
 %% ===== Toolbox settings ================================================
+conf = SFS_config;
 conf.resolution = 1000;
 conf.xref = [0 1 0];
-conf.plot.useplot = false;
-conf.c = 343;
 conf.phase = -0.8;
-conf.usenormalisation = true;
-conf.showprogress = true;
 
 %% ===== Main ============================================================
 [P,x,y,z] = sound_field_mono_line_source(X,Y,Z,xs,f,conf);
+P = normalization(P,'line',f,conf);
 gp_save_matrix('data/sound_field_line_source.dat',x,y,real(P));
+
+rmpath('../../matlab');
