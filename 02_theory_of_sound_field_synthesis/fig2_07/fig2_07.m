8cc1416cf1bc20781e40db195c3d9275b13c7309
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-24 09:32:29 +0100

    Update Fig. 2.7 to SFS 2.4.0

--- 02_theory_of_sound_field_synthesis/fig2_07/fig2_07.m
+++ 02_theory_of_sound_field_synthesis/fig2_07/fig2_07.m
@@ -10,36 +10,25 @@ xs = [0 -1 0];
 src = 'pw';
 f = 1000; % / Hz
 
-
 %% ===== Toolbox settings ================================================
+conf = SFS_config;
 conf.resolution = 1000; % / samples
-conf.xref = [0 0 0]; % / m
-conf.dimension = '2.5D';
-conf.c = 343; % / m/s
-conf.driving_functions = 'default';
-conf.phase = 0; % / rad
-conf.usenormalisation = true;
-conf.plot.useplot = false;
 conf.showprogress = true;
-conf.nfchoa.order = [];
-
 
 %% ===== Secondary Sources ===============================================
 conf.secondary_sources.size = 3; % / m
 conf.secondary_sources.center = [0 0 0]; % / m
 conf.secondary_sources.geometry = 'circle';
 conf.secondary_sources.number = 1000;
-conf.secondary_sources.x0 = [];
-% get secondary sources and store them, to do this calculation only once
-x0 = secondary_source_positions(conf);
-conf.secondary_sources.x0 = x0;
 % store complete array and repeat the first loudspeaker in order to draw a
 % complete circle
+x0 = secondary_source_positions(conf);
 gp_save_loudspeakers('data/array.txt',[x0; x0(1,:)]);
-
 
 %% ===== Near-field compensated higer order Ambisonics ===================
 [P,x,y] = sound_field_mono_nfchoa(X,Y,Z,xs,src,f,conf);
 file = sprintf('data/sound_field_nfchoa_pw_f%iHz_nls%i.dat',f, ...
                conf.secondary_sources.number);
 gp_save_matrix(file,x,y,real(P));
+
+rmpath('../../matlab');
