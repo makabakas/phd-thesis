017bde6e22e2062feb18485803670595f88a8582
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-24 09:46:58 +0100

    Update Fig. 2.8 to SFS 2.4.0

--- 02_theory_of_sound_field_synthesis/fig2_08/fig2_08.m
+++ 02_theory_of_sound_field_synthesis/fig2_08/fig2_08.m
@@ -11,34 +11,27 @@ src = 'ps';
 f = 1000; % / Hz
 
 %% ===== Toolbox settings ================================================
+conf = SFS_config;
 conf.resolution = 1000; % / samples
-conf.xref = [0 0 0]; % / m
-conf.dimension = '2.5D';
-conf.c = 343; % / m/s
-conf.driving_functions = 'default';
-conf.phase = 0; % / rad
-conf.usenormalisation = true;
-conf.plot.useplot = false;
 conf.showprogress = true;
-conf.nfchoa.order = [];
 
 %% ===== Secondary Sources ===============================================
 conf.secondary_sources.size = 3; % / m
 conf.secondary_sources.center = [0 0 0]; % / m
 conf.secondary_sources.geometry = 'circle';
-conf.secondary_sources.number = 1000;
-conf.secondary_sources.x0 = [];
-% get secondary sources and store them, to do this calculation only once
-x0 = secondary_source_positions(conf);
-% store complete array and repeat the first loudspeaker in order to draw a
+% Store complete array and repeat the first loudspeaker in order to draw a
 % complete circle
-gp_save_loudspeakers('data/array.txt',[x0; x0(1,:)]);
-% use fewer secondary sources, because otherwise we will get -infty
-conf.secondary_sources.number = 64;
+conf.secondary_sources.number = 1000;
 x0 = secondary_source_positions(conf);
-conf.secondary_sources.x0 = x0;
+gp_save_loudspeakers('data/array.txt',[x0; x0(1,:)]);
+% Use high number of secondary sources only for plotting, for calculation a
+% lower number is sufficient as the alisaing frequency is high enough.
+conf.secondary_sources.number = 64;
 
 %% ===== Near-field compensated higer order Ambisonics ===================
 [P,x,y] = sound_field_mono_nfchoa(X,Y,Z,xs,src,f,conf);
+P = normalization(P,'point');
 file = sprintf('data/sound_field_nfchoa_ps_f%iHz_nls%i.dat',f,conf.secondary_sources.number);
 gp_save_matrix(file,x,y,real(P));
+
+rmpath('../../matlab');
