993bde2e66082cc62aa2d8daa6b7d8817a8398be
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-25 18:08:45 +0100

    Update Fig. 3.16 to SFS 2.4.0

--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_16/fig3_16.m
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_16/fig3_16.m
@@ -1,57 +1,41 @@
-% create sound field simulations for a linear array and WFS with and without
-% tapering window to show the edge waves
 clear all;
 addpath('../../matlab');
 create_dir('data');
 
 %% ===== Configuration ===================================================
+conf = SFS_config;
 X = [0 0.75 0]; % / m
 Y = [0 0 -0.75]; % / m
 Z = [0 0 0]; % / m
-t = (1:500)'; % / samples
+t = (1:500)'/conf.fs; % / samples
 xs = [0 0.5 0 0 -1 0];
 src = 'fs';
 
 %% ===== Toolbox settings ================================================
-conf.debug = false;
 conf.resolution = 1000; % / samples
-conf.xref = [0 0 0]; % / m
-conf.dimension = '2.5D';
-conf.c = 343; % / m/s
-conf.fs = 44100; % / Hz
 conf.N = 1024;
-conf.wfs.usehpre = true;
-conf.wfs.hprefhigh = 20000;
 conf.wfs.hpreflow = 1;
-conf.wfs.hpretype = 'FIR';
-conf.nfchoa.order = [];
-conf.usebandpass = true;
-conf.bandpassflow = 10;
-conf.bandpassfhigh = 20000;
-conf.usefracdelay = true;
-conf.fracdelay_method = 'resample';
-conf.driving_functions = 'default';
-conf.phase = 0; % / rad
-conf.usenormalisation = false;
-conf.plot.useplot = false;
+conf.wfs.hprefhigh = 20000;
 conf.usetapwin = false;
-conf.tapwinlen = 0.3;
-conf.showprogress = false;
+conf.delayline.resampling = 'pm';
+conf.delayline.resamplingfactor = 8;
+conf.delayline.resamplingorder = 64;
+conf.delayline.filter = 'lagrange';
+conf.delayline.filterorder = 9;  
 
 %% ===== Secondary Sources ===============================================
 conf.secondary_sources.size = 3;
 conf.secondary_sources.geometry = 'circle';
 conf.secondary_sources.center = [0 0 0];
-conf.secondary_sources.x0 = [];
 
 %% ===== Time signal ======================================================
-% get time signal to visualize driving signals
+% Get time signal to visualize driving signals
 pulse = hann_window(5,5,10);
 
 %% ===== Continuous secondary sources =====================================
 conf.secondary_sources.number = 500;
 x0 = secondary_source_positions(conf);
-% --- WFS
+% --- WFS ---
 x0 = secondary_source_selection(x0,xs,src);
 x0 = secondary_source_tapering(x0,conf);
 d = driving_function_imp_wfs(x0,xs,src,conf);
@@ -60,17 +44,17 @@ for jj=1:length(X)
     p = zeros(size(t));
     for ii=1:length(t)
         progress_bar(ii,length(t))
-        % add 64 samples offset to the time because of the pre-equalization
+        % Add 64 samples offset to the time because of the pre-equalization
         % filter
-        p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii)+64,conf);
+        p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii)+64/conf.fs,conf);
     end
-    gp_save(sprintf('data/wfs_X%.2f_Y%.2f.txt',X(jj),Y(jj)),[t/conf.fs p]);
+    gp_save(sprintf('data/wfs_X%.2f_Y%.2f.txt',X(jj),Y(jj)),[t p]);
 end
 
 %% ===== Sampled swecondary sources ======================================
 conf.secondary_sources.number = 64;
 x0 = secondary_source_positions(conf);
-% --- WFS
+% --- WFS ---
 x0 = secondary_source_selection(x0,xs,src);
 x0 = secondary_source_tapering(x0,conf);
 d = driving_function_imp_wfs(x0,xs,src,conf);
@@ -79,10 +63,11 @@ for jj=1:length(X)
     p = zeros(size(t));
     for ii=1:length(t)
         progress_bar(ii,length(t))
-        % add 64 samples offset to the time because of the pre-equalization
+        % Add 64 samples offset to the time because of the pre-equalization
         % filter
-        p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii)+64,conf);
+        p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii)+64/conf.fs,conf);
     end
-    gp_save(sprintf('data/wfs_X%.2f_Y%.2f_nls64.txt',X(jj),Y(jj)), ...
-            [t/conf.fs p]);
+    gp_save(sprintf('data/wfs_X%.2f_Y%.2f_nls64.txt',X(jj),Y(jj)),[t p]);
 end
+
+rmpath('../../matlab');
--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_16/fig3_16.plt
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_16/fig3_16.plt
@@ -38,18 +38,18 @@ set label 4 '\scs $(0,0,0)$\,m'     at 0.7,-0.4 left front tc ls 101
 set label 5 '\scs $(0.75,0,0)$\,m'  at 0.7,-2.4 left front tc ls 101
 set label 6 '\scs $(0,-0.75,0)$\,m' at 0.7,-4.4 left front tc ls 101
 stats 'wfs_X0.00_Y0.00.txt' u 2 nooutput
-plot [*:*] [-5:1] 'wfs_X0.00_Y0.00.txt'  u ($1*1000):($2/STATS_min)   w l ls 1 lw 2,\
-                  'wfs_X0.75_Y0.00.txt'  u ($1*1000):($2/STATS_min-2) w l ls 2 lw 2,\
-                  'wfs_X0.00_Y-0.75.txt' u ($1*1000):($2/STATS_min-4) w l ls 3 lw 2
+plot [*:*] [-5:1] 'wfs_X0.00_Y0.00.txt'  u ($1*1000):($2/STATS_max)   w l ls 1 lw 2,\
+                  'wfs_X0.75_Y0.00.txt'  u ($1*1000):($2/STATS_max-2) w l ls 2 lw 2,\
+                  'wfs_X0.00_Y-0.75.txt' u ($1*1000):($2/STATS_max-4) w l ls 3 lw 2
 unset for [ii=3:6] label ii
 load 'xborder.cfg'
 set lmargin 2
 set rmargin 4
 set label 1 '\ft 64 secondary sources'
 stats 'wfs_X0.00_Y0.00_nls64.txt' u 2 nooutput
-plot [*:*] [-5:1] 'wfs_X0.00_Y0.00_nls64.txt'  u ($1*1000):($2/STATS_min)   w l ls 1 lw 2,\
-                  'wfs_X0.75_Y0.00_nls64.txt'  u ($1*1000):($2/STATS_min-2) w l ls 2 lw 2,\
-                  'wfs_X0.00_Y-0.75_nls64.txt' u ($1*1000):($2/STATS_min-4) w l ls 3 lw 2
+plot [*:*] [-5:1] 'wfs_X0.00_Y0.00_nls64.txt'  u ($1*1000):($2/STATS_max)   w l ls 1 lw 2,\
+                  'wfs_X0.75_Y0.00_nls64.txt'  u ($1*1000):($2/STATS_max-2) w l ls 2 lw 2,\
+                  'wfs_X0.00_Y-0.75_nls64.txt' u ($1*1000):($2/STATS_max-4) w l ls 3 lw 2
 unset multiplot
 
 call 'plot.plt' 'fig3_16'
