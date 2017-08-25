0f2158d48fec7f5e2c4331482b7e213ec95eddb2
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-25 18:10:29 +0100

    Update Fig. 3.14 to SFS 2.4.0

--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_14/fig3_14.m
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_14/fig3_14.m
@@ -1,68 +1,54 @@
-% create sound field simulations for a linear array and WFS with and without
-% tapering window to show the edge waves
 clear all;
 addpath('../../matlab');
 create_dir('data');
 
 %% ===== Configuration ===================================================
+conf = SFS_config;
 X = [0 0.75 0]; % / m
 Y = [0 0 0.75]; % / m
 Z = [0 0 0]; % / m
-t = (1:500)'; % / samples
+t = (1:500)'/conf.fs; % / s
 xs = [0 -1 0];
 src = 'pw';
+runs = length(X)*length(t);
 
 %% ===== Toolbox settings ================================================
-conf.debug = false;
 conf.resolution = 1000; % / samples
-conf.xref = [0 0 0]; % / m
-conf.dimension = '2.5D';
-conf.c = 343; % / m/s
-conf.fs = 44100; % / Hz
 conf.N = 1024;
-conf.wfs.usehpre = true;
-conf.wfs.hprefhigh = 20000;
 conf.wfs.hpreflow = 1;
-conf.wfs.hpretype = 'FIR';
-conf.nfchoa.order = [];
-conf.usebandpass = true;
-conf.bandpassflow = 10;
-conf.bandpassfhigh = 20000;
-conf.usefracdelay = true;
-conf.fracdelay_method = 'resample';
-conf.driving_functions = 'default';
-conf.phase = 0; % / rad
-conf.usenormalisation = false;
-conf.plot.useplot = false;
+conf.wfs.hprefhigh = 20000;
 conf.usetapwin = false;
-conf.tapwinlen = 0.3;
-conf.showprogress = false;
+conf.delayline.resampling = 'pm';
+conf.delayline.resamplingfactor = 8;
+conf.delayline.resamplingorder = 64;
+conf.delayline.filter = 'lagrange';
+conf.delayline.filterorder = 9;
 
 %% ===== Secondary Sources ===============================================
 conf.secondary_sources.size = 3;
 conf.secondary_sources.geometry = 'circle';
 conf.secondary_sources.center = [0 0 0];
-conf.secondary_sources.x0 = [];
 
 %% ===== Time signal ======================================================
-% get time signal to visualize driving signals
+% Get time signal to visualize driving signals
 pulse = hann_window(5,5,10);
 
 %% ===== Continuous secondary sources =====================================
 conf.secondary_sources.number = 500;
+conf.nfchoa.order = 250;
 x0 = secondary_source_positions(conf);
-% --- NFCHOA
+% --- NFC-HOA ---
 d = driving_function_imp_nfchoa(x0,xs,src,conf);
 d = convolution(d,pulse);
 for jj=1:length(X)
     p = zeros(size(t));
     for ii=1:length(t)
-        progress_bar(ii,length(t))
         p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii),conf);
+        progress_bar(ii+(jj-1)*length(t),5*runs);
     end
-    gp_save(sprintf('data/nfchoa_X%.2f_Y%.2f.txt',X(jj),Y(jj)),[t/conf.fs p]);
+    gp_save(sprintf('data/nfchoa_X%.2f_Y%.2f.txt',X(jj),Y(jj)),[t p]);
 end
-% --- WFS
+% --- WFS ---
 x0 = secondary_source_selection(x0,xs,src);
 x0 = secondary_source_tapering(x0,conf);
 d = driving_function_imp_wfs(x0,xs,src,conf);
@@ -70,44 +56,41 @@ d = convolution(d,pulse);
 for jj=1:length(X)
     p = zeros(size(t));
     for ii=1:length(t)
-        progress_bar(ii,length(t))
-        % add 64 samples offset to the time because of the pre-equalization
+        % Add 64 samples offset to the time because of the pre-equalization
         % filter
-        p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii)+64,conf);
+        p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii)+64/conf.fs,conf);
+        progress_bar(ii+(jj-1)*length(t)+runs,5*runs);
     end
-    gp_save(sprintf('data/wfs_X%.2f_Y%.2f.txt',X(jj),Y(jj)),[t/conf.fs p]);
+    gp_save(sprintf('data/wfs_X%.2f_Y%.2f.txt',X(jj),Y(jj)),[t p]);
 end
 
-%% ===== Sampled swecondary sources ======================================
+%% ===== Sampled secondary sources =======================================
 conf.secondary_sources.number = 64;
 x0 = secondary_source_positions(conf);
-% --- NFCHOA
-conf.nfchoa.order = 256;
+% --- NFC-HOA ---
 d = driving_function_imp_nfchoa(x0,xs,src,conf);
 d = convolution(d,pulse);
 for jj=1:length(X)
     p = zeros(size(t));
     for ii=1:length(t)
-        progress_bar(ii,length(t))
         p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii),conf);
+        progress_bar(ii+(jj-1)*length(t)+2*runs,5*runs);
     end
-    gp_save(sprintf('data/nfchoa_X%.2f_Y%.2f_nls64.txt', ...
-                    X(jj),Y(jj)),[t/conf.fs p]);
+    gp_save(sprintf('data/nfchoa_X%.2f_Y%.2f_nls64.txt',X(jj),Y(jj)),[t p]);
 end
-% --- NFCHOA band limited
+% --- NFC-HOA band limited ---
 conf.nfchoa.order = [];
 d = driving_function_imp_nfchoa(x0,xs,src,conf);
 d = convolution(d,pulse);
 for jj=1:length(X)
     p = zeros(size(t));
     for ii=1:length(t)
-        progress_bar(ii,length(t))
         p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii),conf);
+        progress_bar(ii+(jj-1)*length(t)+3*runs,5*runs);
     end
-    gp_save(sprintf('data/nfchoa_X%.2f_Y%.2f_nls64_band_limited.txt', ...
-                    X(jj),Y(jj)),[t/conf.fs p]);
+    gp_save(sprintf('data/nfchoa_X%.2f_Y%.2f_nls64_band_limited.txt',X(jj),Y(jj)),[t p]);
 end
-% --- WFS
+% --- WFS ---
 x0 = secondary_source_selection(x0,xs,src);
 x0 = secondary_source_tapering(x0,conf);
 d = driving_function_imp_wfs(x0,xs,src,conf);
@@ -115,11 +98,12 @@ d = convolution(d,pulse);
 for jj=1:length(X)
     p = zeros(size(t));
     for ii=1:length(t)
-        progress_bar(ii,length(t))
-        % add 64 samples offset to the time because of the pre-equalization
+        % Add 64 samples offset to the time because of the pre-equalization
         % filter
-        p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii)+64,conf);
+        p(ii) = sound_field_imp(X(jj),Y(jj),Z(jj),x0,'ps',d,t(ii)+64/conf.fs,conf);
+        progress_bar(ii+(jj-1)*length(t)+4*runs,5*runs);
     end
-    gp_save(sprintf('data/wfs_X%.2f_Y%.2f_nls64.txt', ...
-                    X(jj),Y(jj)),[t/conf.fs p]);
+    gp_save(sprintf('data/wfs_X%.2f_Y%.2f_nls64.txt',X(jj),Y(jj)),[t p]);
 end
+
+rmpath('../../matlab');
--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_14/fig3_14.plt
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_14/fig3_14.plt
@@ -35,7 +35,7 @@ set rmargin 0
 set label 1 '\ft continuous secondary sources' at 6,1.4 center tc ls 101
 set label 2 '\ft \acs{NFC-HOA}' at 11.8,-4.6 right front tc ls 101
 stats 'nfchoa_X0.00_Y0.00.txt' u 2 nooutput
-plot [*:*] [-5:1] 'nfchoa_X0.00_Y0.00.txt' u ($1*1000):($2/STATS_max) w l ls 1 lw 2, \
+plot [*:*] [-5:1] 'nfchoa_X0.00_Y0.00.txt' u ($1*1000):($2/STATS_max)   w l ls 1 lw 2, \
                   'nfchoa_X0.75_Y0.00.txt' u ($1*1000):($2/STATS_max-2) w l ls 2 lw 2, \
                   'nfchoa_X0.00_Y0.75.txt' u ($1*1000):($2/STATS_max-4) w l ls 3 lw 2
 set origin 0.335,0.5
@@ -44,17 +44,10 @@ unset ylabel
 set lmargin 3.5
 set rmargin 3.5
 set label 1 '\ft 64 secondary sources'
-set label 3 '\tiny numerically unstable' at 7.50,-1.93 front tc ls 2
-set label 4 '\tiny numerically unst.' at 8.22,-3.93 front tc ls 3
-limit1(x,y) = x>7.25 ? 1/0 : y # this hides the numerical unstable parts
-limit2(x,y) = x>7.97 ? 1/0 : y
 stats 'nfchoa_X0.00_Y0.00_nls64.txt' u 2 nooutput
-plot [*:*] [-5:1] 'nfchoa_X0.00_Y0.00_nls64.txt' \
-    u ($1*1000):($2/STATS_max) w l ls 1 lw 2,\
-                  'nfchoa_X0.75_Y0.00_nls64.txt' \
-    u ($1*1000):(limit1($1*1000,$2/STATS_max-2)) w l ls 2 lw 2,\
-                  'nfchoa_X0.00_Y0.75_nls64.txt' \
-    u ($1*1000):(limit2($1*1000,$2/STATS_max-4)) w l ls 3 lw 2
+plot [*:*] [-5:1] 'nfchoa_X0.00_Y0.00_nls64.txt' u ($1*1000):($2/STATS_max)   w l ls 1 lw 2,\
+                  'nfchoa_X0.75_Y0.00_nls64.txt' u ($1*1000):($2/STATS_max-2) w l ls 2 lw 2,\
+                  'nfchoa_X0.00_Y0.75_nls64.txt' u ($1*1000):($2/STATS_max-4) w l ls 3 lw 2
 set origin 0.67,0.5
 unset label 3
 unset label 4
@@ -64,12 +57,9 @@ set lmargin 0
 set rmargin 7
 set label 2 '\ft band-limited \acs{NFC-HOA}'
 stats 'nfchoa_X0.00_Y0.00_nls64_band_limited.txt' u 2 nooutput
-plot [*:*] [-5:1] 'nfchoa_X0.00_Y0.00_nls64_band_limited.txt' \
-    u ($1*1000):($2/STATS_max) w l ls 1 lw 2,\
-                  'nfchoa_X0.75_Y0.00_nls64_band_limited.txt' \
-    u ($1*1000):($2/STATS_max-2) w l ls 2 lw 2,\
-                  'nfchoa_X0.00_Y0.75_nls64_band_limited.txt' \
-    u ($1*1000):($2/STATS_max-4) w l ls 3 lw 2
+plot [*:*] [-5:1] 'nfchoa_X0.00_Y0.00_nls64_band_limited.txt' u ($1*1000):($2/STATS_max)   w l ls 1 lw 2,\
+                  'nfchoa_X0.75_Y0.00_nls64_band_limited.txt' u ($1*1000):($2/STATS_max-2) w l ls 2 lw 2,\
+                  'nfchoa_X0.00_Y0.75_nls64_band_limited.txt' u ($1*1000):($2/STATS_max-4) w l ls 3 lw 2
 set origin 0,0
 load 'xyborder.cfg'
 set ylabel 'sound pressure'
@@ -84,21 +74,18 @@ set label 4 '\scs $(0,0,0)$\,m' at 11,0.5 right front tc ls 101
 set label 5 '\scs $(0.75,0,0)$\,m' at 11,-1.5 right front tc ls 101
 set label 6 '\scs $(0,0.75,0)$\,m' at 11,-3.5 right front tc ls 101
 stats 'wfs_X0.00_Y0.00.txt' u 2 nooutput
-plot [*:*] [-5:1] 'wfs_X0.00_Y0.00.txt' u ($1*1000):($2/STATS_min) w l ls 1 lw 2,\
-                  'wfs_X0.75_Y0.00.txt' u ($1*1000):($2/STATS_min-2) w l ls 2 lw 2,\
-                  'wfs_X0.00_Y0.75.txt' u ($1*1000):($2/STATS_min-4) w l ls 3 lw 2
+plot [*:*] [-5:1] 'wfs_X0.00_Y0.00.txt' u ($1*1000):($2/STATS_max)   w l ls 1 lw 2,\
+                  'wfs_X0.75_Y0.00.txt' u ($1*1000):($2/STATS_max-2) w l ls 2 lw 2,\
+                  'wfs_X0.00_Y0.75.txt' u ($1*1000):($2/STATS_max-4) w l ls 3 lw 2
 set origin 0.335,0
 unset for [ii=3:6] label ii
 load 'xborder.cfg'
 set lmargin 3.5
 set rmargin 3.5
 stats 'wfs_X0.00_Y0.00_nls64.txt' u 2 nooutput
-plot [*:*] [-5:1] 'wfs_X0.00_Y0.00_nls64.txt' \
-    u ($1*1000):($2/STATS_min) w l ls 1 lw 2,\
-                  'wfs_X0.75_Y0.00_nls64.txt' \
-    u ($1*1000):($2/STATS_min-2) w l ls 2 lw 2,\
-                  'wfs_X0.00_Y0.75_nls64.txt' \
-    u ($1*1000):($2/STATS_min-4) w l ls 3 lw 2
+plot [*:*] [-5:1] 'wfs_X0.00_Y0.00_nls64.txt' u ($1*1000):($2/STATS_max)   w l ls 1 lw 2,\
+                  'wfs_X0.75_Y0.00_nls64.txt' u ($1*1000):($2/STATS_max-2) w l ls 2 lw 2,\
+                  'wfs_X0.00_Y0.75_nls64.txt' u ($1*1000):($2/STATS_max-4) w l ls 3 lw 2
 unset multiplot
 
 call 'plot.plt' 'fig3_14'
