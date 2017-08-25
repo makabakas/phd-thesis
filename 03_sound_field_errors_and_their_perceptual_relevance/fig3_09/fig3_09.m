b0afd1a370ba0de400a6158a2c3cd568097fc7c0
Hagen Wierstorf <hagen.wierstorf@tu-berlin.de>
2017-08-25 15:56:42 +0100

    Update Fig. 3.9 to SFS 2.4.0

--- 03_sound_field_errors_and_their_perceptual_relevance/fig3_09/fig3_09.m
+++ 03_sound_field_errors_and_their_perceptual_relevance/fig3_09/fig3_09.m
@@ -11,16 +11,9 @@ xs = [0 0.5 0 0 -1 0];
 src = 'fs';
 
 %% ===== Toolbox settings ================================================
+conf = SFS_config;
 conf.resolution = 1000; % / samples
-conf.xref = [0 0 0]; % / m
-conf.dimension = '2.5D';
-conf.c = 343; % / m/s
-conf.driving_functions = 'default';
-conf.phase = 0; % / rad
-conf.usenormalisation = true;
-conf.plot.useplot = false;
 conf.usetapwin = false;
-conf.tapwinlen = 0.3;
 conf.showprogress = true;
 
 %% ===== Secondary Sources ===============================================
@@ -28,20 +21,24 @@ conf.secondary_sources.size = 3; % / m
 conf.secondary_sources.center = [0 0 0]; % / m
 conf.secondary_sources.geometry = 'circle';
 conf.secondary_sources.number = 64;
-conf.secondary_sources.x0 = [];
 x0 = secondary_source_positions(conf);
 gp_save_loudspeakers('data/array.txt',x0);
 
 %% ===== without aliasing ================================================
 f = 1000; % / Hz
 [P,x,y,~,x0] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
+P = normalization(P,'point');
 gp_save_matrix('data/sound_field_fs_1000Hz.dat',x,y,real(P));
 gp_save_loudspeakers('data/array_wfs.txt',x0);
 
 %% ===== with aliasing ===================================================
 f = 2000; % / Hz
 [P,x,y] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
+P = normalization(P,'point');
 gp_save_matrix('data/sound_field_fs_2000Hz.dat',x,y,real(P));
 f = 5000; % / Hz
 [P,x,y] = sound_field_mono_wfs(X,Y,Z,xs,src,f,conf);
+P = normalization(P,'point');
 gp_save_matrix('data/sound_field_fs_5000Hz.dat',x,y,real(P));
+
+rmpath('../../matlab');
